ce0e4513107a21939dd57f38f386c09ebc200d51
thinhcoder21 <thinh.haphuc18@gmail.com>
2023-10-27 11:31:35 +0700

    da xu ly xong :))

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -95,6 +95,7 @@ namespace Calculator
             this.button1.TabIndex = 6;
             this.button1.Text = "total";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label4
             // 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -21,5 +21,14 @@ namespace Calculator
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = a + b;
+            textBox3.Text = c.ToString();
+
+        }
     }
 }
